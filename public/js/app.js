ddc57e5c6fac3ae10af857a1bef4a9e5375cb9c4
asantanacu <asantanacu@gmail.com>
2016-12-02 10:18:06 -0500

    Included app.js file

---
+++ public/js/app.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+	console.log("Revelex app is ready");
+})
--- views/layout.php
+++ views/layout.php
@@ -66,8 +66,8 @@
 
 	<?= $this->content('main.content');   ?>
 
-    <script src="/app.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="/js/app.js"></script>
 </body>
 </html>
